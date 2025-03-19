dd24bd74586468fb935ae9fcde29cec3db5401de
Xavi Garcia <xavi.garcia@roche.com>
2025-03-19 12:58:32 +0100

    added more test cases for the project mapper

--- src/pods/project/project.mapper.spec.ts
+++ src/pods/project/project.mapper.spec.ts
@@ -11,6 +11,21 @@ const API_MODEL_PROJECT : apiModel.Project = {
     employees: []
 };
 
+const API_MODEL_PROJECT_WITH_EMPLOYEES : apiModel.Project = {
+    id: '2',
+    name: 'Second Project',
+    externalId: undefined,
+    comments: undefined,
+    isActive: true,
+    employees: [
+      {
+        id: '123',
+        isAssigned: true,
+        employeeName: 'John Smith',
+      }
+    ],
+};
+
 const EMPTY_PROJECT : viewModel.Project = {
     id: '',
     name: '',
@@ -22,6 +37,7 @@ const EMPTY_PROJECT : viewModel.Project = {
 
 describe('Project mapper tests', () => {
     describe('map project from API to VM', () => {
+
         it('should map the project entity from the api model to the vm model', () => {
 
             let result:viewModel.Project = projectMapper.mapProjectFromApiToVm(API_MODEL_PROJECT);
@@ -29,9 +45,16 @@ describe('Project mapper tests', () => {
             expect(result).toEqual(API_MODEL_PROJECT);
         });
 
-        it('should create an empty project when the project is not provided', () => {
+        it('should map the project entity from the api model to the vm model with employees', () => {
 
-            let result:viewModel.Project = projectMapper.mapProjectFromApiToVm(undefined);
+            let result:viewModel.Project = projectMapper.mapProjectFromApiToVm(API_MODEL_PROJECT_WITH_EMPLOYEES);
+
+            expect(result).toEqual(API_MODEL_PROJECT_WITH_EMPLOYEES);
+        });
+
+        it.each([null, undefined])('should create an empty project when the project is not provided, %s', (testCase) => {
+            
+            let result:viewModel.Project = projectMapper.mapProjectFromApiToVm(testCase);
 
             expect(result).toEqual(EMPTY_PROJECT);
         });
